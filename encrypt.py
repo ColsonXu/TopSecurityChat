008b2417f62401db83d767858e9ab787187a4842
Colson Xu <colson.xu@gmail.com>
2017-08-24 15:01:47 -0700

    Seperated Keygen in an individual class.

--- encrypt.py
+++ encrypt.py
@@ -3,37 +3,26 @@ import random as r
 rand_seed = input('Please enter the private key of this message: ')
 raw_message = input('Please enter the message: ')
 
+raw_message_list = []
+for ch in raw_message:
+	raw_message_list.append(ch)
 
-def encrypt(r_str, rand_seed):
-	r.seed(rand_seed)
-	generated_key = str(r.randint(1000000000000000000000000000000000000000000000,9999999999999999999999999999999999999999999999))
-	encrypt_list = [generated_key[i:i+2] for i in range(0, len(generated_key), 2)]
-	print(encrypt_list) # Debug line
-	encrypted_string = reversed(r_str)
+r.seed(rand_seed)
+generated_key = str(r.randint(10 ** 45, 10 ** 46 - 1))
+
+def dict_key_generator(key):
+	encrypt_list = [key[i:i+2] for i in range(0, len(key), 2)]
+	return encrypt_list
+
+def Resersion(r_str, rand_seed):
+	dict_key_generator(generated_key)
+	encrypted_string = r_str[::-1]
+	msg_idx = 0
+	lst_idx = 0
+	for ch in range(len(r_str)):
+		encrypted_string.insert(msg_idx, ENCRYPTION_DICTIONARY[ encrypt_list[lst_idx] ])
 	return encrypted_string
 
 
-print('Your encrypted message is: ' + encrypt(raw_message, rand_seed))
+print('Your encrypted message is: ' + Resersion(raw_message_list, rand_seed))
 
-
-# Encryption Dictionary, DO NOT TEMPER WITH!!!
-
-##############################################################################
-
-ENCRYPTION_DICTIONARY = {'01':'@', '02':'#', '03':'^', '04':'!',
-
-			'05':'s', '06':'a', '07':'d', '08':'x',
-			'09':'r', '10':'h', '11':'b', '12':'v',
-			'13':'n', '14':'z', '15':'q', '16':'w',
-			'17':'y', '18':'u', '19':'k', '20':'m',
-			'21':'e', '22':'p', '23':'l', '24':'i',
-			'25':'f', '26':'g', '27':'j', '28':'c',
-			'29':'t', '30':'o', '31':'s',
-
-			'32':'2', '33':'3', '34':'5', '35':'1',
-			'36':'4', '37':'7', '38':'9', '39':'8',
-			'40':'6', '41':'0', '42':'apple', '43':'Sunday',
-			'44':'send', '45':'sandbox',
-			}
-
-#############################################################################
